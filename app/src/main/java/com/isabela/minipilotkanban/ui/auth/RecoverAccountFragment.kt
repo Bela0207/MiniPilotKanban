74fe668a99b25dd3a8c9622586ec48832f166f31
estevaohth <53195572+JoyousWinner@users.noreply.github.com>
2025-06-26 19:54:19 -0300

    Initial commit

---
+++ app/src/main/java/com/isabela/minipilotkanban/ui/auth/RecoverAccountFragment.kt
@@ -0,0 +1,43 @@
+package com.isabela.minipilotkanban.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.isabela.minipilotkanban.R
+import com.isabela.minipilotkanban.databinding.FragmentRecoverAccountBinding
+
+
+class RecoverAccountFragment : Fragment() {
+    private var _binding: FragmentRecoverAccountBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener(){
+        binding.seta.setOnClickListener{
+            findNavController().navigate(R.id.action_recoverAccountFragment_to_loginFragment)
+        }
+    }
+}
